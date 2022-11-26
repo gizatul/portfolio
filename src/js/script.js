7c2d03ec9d3dc75b9e99f52cf6e1e1a55b4044d9
gizatul <gizat-aidar@yandex.ru>
2022-11-26 19:50:26 +0500

    project done

--- src/js/script.js
+++ src/js/script.js
@@ -27,3 +27,22 @@ const counters = document.querySelectorAll('.skills__scale-value'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+// Smooth scroll and pageup
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href=up]").click(function() {
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
+  new WOW().init();
+
